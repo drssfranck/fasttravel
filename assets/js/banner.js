39ae4c5aa96f8bf012a11b607fb3090fb45e14c6
marc007-ux <noumsimarc007@gmail.com>
2025-08-07 19:05:52 +0300

    banner

---
+++ assets/js/banner.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const slides = document.querySelectorAll(".slide"); 
+    let currentSlide = 0; 
+    const slideInterval = 5000; 
+
+    function showSlide(index) {
+    
+        slides.forEach(slide => slide.classList.remove("active"));
+        
+        slides[index].classList.add("active");
+    }
+
+    function nextSlide() {
+        
+        currentSlide = (currentSlide + 1) % slides.length;
+        showSlide(currentSlide);
+    }
+
+    
+    setInterval(nextSlide, slideInterval);
+
+    
+    showSlide(currentSlide);
+});
